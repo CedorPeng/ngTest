ee5fe497aa2b6cf381d7f0bccb291557f4a14270
CedorPeng <17600834351@163.com>
2019-04-14 14:21:02 +0800

    reset

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'routerNg'`, () => {
+  it(`should have as title 'ngTest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('routerNg');
+    expect(app.title).toEqual('ngTest');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to routerNg!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngTest!');
   });
 });
