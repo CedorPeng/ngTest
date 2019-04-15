8e9e90bc67d1c075c2c33347d064b3d4d0a436ed
CedorPeng <17600834351@163.com>
2019-04-15 10:41:51 +0800

    初步使用ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { UtilsService } from './services/utils.service';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -17,9 +24,12 @@ import { UtilsService } from './services/utils.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgZorroAntdModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [UtilsService],
+  providers: [UtilsService, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient,HttpHeaders } from "@angular/common/http";
 import { UtilsService } from '../../services/utils.service';
-import { CedorUtils } from '../../../assets/js/utils'
+import { CedorUtils } from '../../../assets/js/utils';
+import { NzMessageService } from 'ng-zorro-antd';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  imgUrl='test/upFile';
+  upLoading=false;
 
   private headers = new HttpHeaders({'Content-Type': 'application/json'});
   private utils:any;
+  // provide: NZ_MESSAGE_CONFIG, useValue: { nzDuration: 3000 }
 
-  constructor(private http:HttpClient,private utilService:UtilsService) {
+
+  constructor(private http:HttpClient,private utilService:UtilsService,private message: NzMessageService) {
     this.utils = new CedorUtils
   }
 
@@ -22,18 +27,21 @@ export class HomeComponent implements OnInit {
     // this.http.get('test/test?msg='+'111').subscribe(res=>{
     //   console.log(res);
     // })
-    this.http.post('test/post',{msg:'11111'} , {headers:this.headers}).subscribe(res=>{
-      console.log(res);
-    })
+    // this.http.post('test/post',{msg:'11111'} , {headers:this.headers}).subscribe(res=>{
+    //   console.log(res);
+    // })
     console.log(this.utilService.getApproveType(100));
   }
-  upFile(e){
-    console.log(e);
-    let data = new FormData()
-    data.append('file',e.srcElement.files[0])
-    this.http.post('test/upFile',data).subscribe(res=>{
-      console.log(res);
-    })
+  upData(e){
+    this.upLoading =true;
+    if(e.type != 'progress' && e.type != 'start' ){
+      let type = e.type == 'success'? '上传成功':'上传失败';
+      let msgFn = e.type == 'success'? 'success':'error';
+
+      this.message[msgFn](type,{nzDuration:4000})
+      this.upLoading =false;
+    }
+
   }
 
 }
